53ea647ff29bbf18437f0127b54183feec700e06
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-21 22:23:01 +0530

    [ADDED]: shubham This code for Data type Demo

---
+++ DataTypeProgram.py
@@ -0,0 +1,16 @@
+#String Variable
+name = "shubham"
+#integer 
+age = 21
+#float 
+salary = 15000.50
+#Boolean
+old = True
+# None
+a = None
+
+print("My Name is : ",name, type(name))
+print("My age is : ",age ,type(age))
+print("My Salary is : ",salary ,type(salary))
+print("Old : ",old , type(old))
+print("A = ",a , type(a))
