39e4285d2306bd207697029cf6e70d8f31bbade5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-22 15:10:18 +0530

    [ADDED]: shubham This code for If Else Ladder Demo

---
+++ ConditionalStatementIfElseLadder.py
@@ -0,0 +1,10 @@
+light = input("Enter the Light Color : ")
+
+if(light == "Red"):
+    print("Stop !")
+elif(light == "Green"):
+    print("Go !")
+elif(light == "Yellow"):
+    print("Look !")
+else:
+    print("Light Are Broken ! ")
