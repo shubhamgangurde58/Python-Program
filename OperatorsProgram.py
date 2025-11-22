c077f8d57dd2f4ae267b0c2e27de765ffb41610d
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-22 14:20:08 +0530

    [ADDED]: shubham This code for Operator Demo

---
+++ OperatorsProgram.py
@@ -0,0 +1,65 @@
+# Arithmetic Operator + , - , / , * , % , // , ** 
+a = 20
+b = 5
+
+print("\n Arithmetic Operators \n")
+
+print("Addition := ",a+b)
+print("Subtraction := ",a-b)
+print("Division := ", a/b)
+print("Multiplication := ",a*b)
+print("Modules := ",a%b)
+print("Floor Division := ",a//b)
+print("Exponentiation := ",a**b)
+
+# Comparison (Relational) Operators == , != , > , < , >= , <= 
+
+x = 10
+y = 20
+
+print("\n Comparison (Relational) Operators \n")
+
+print("Equal := ",x == y)
+print("Not Equal := ",x != y)
+print("Greater Than := ",x > y)
+print("Less Than := ",x < y)
+print("Greater Than Or Equal To := ",x >= y)
+print("Less Than Or Equal To := ",x <= y)
+
+# Assignment Operators = , += , -= , *= , /= , %= , //= , **= , &= 
+
+'''
+x=10
+y=20
+
+print("/n Assignment Operator /n")
+
+print(" = ",x = y)
+x = 10
+y = 20 
+m = x += 3
+print(" += ",m)
+x,y = 10,5
+m = x -= 4
+print(" -= ",m)
+x,y = 10,5
+m = x *= 5
+print(" *= ",m)
+x,y = 10,5
+m = x /= y
+print(" /= ",m)
+x,y = 10,5
+m= x %= y
+print(" %= ",m)
+x,y = 10,5
+m = x //= y
+print(" //= ",m)
+x,y = 10,5
+m = x **= y
+print(" **= ",m)
+x,y = 10,5
+m = x **= y
+print(" &= ",m)
+x,y = 10,5
+
+'''
