e37fa2073086771e5cfbac62856ce2c0d33dae84
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-22 14:19:32 +0530

    [ADDED]: shubham This code for InputOutputFunction Demo

---
+++ InputOutputFunction.py
@@ -0,0 +1,8 @@
+rollNo = int(input("Enter the RollNo : "))
+name = str(input("Enter the Name : "))
+age = int(input("Enter the Age : "))
+fees = float(input("Enter the Fees : "))
+
+print("My RollNo is ",rollNo,"and My Name is ",name,"and i am ",age,"year old and My College fees is ",fees)
+
+print(f"My RollNo is {rollNo} and my Nme is {name} and i am {age} year old and my college fees is {fees}")
