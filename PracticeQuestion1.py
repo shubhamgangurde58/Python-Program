a29f2e5eab92fa1a81ef9882d1f0ed21e613f6f4
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-24 19:24:17 +0530

    [ADDED]: shubham This code for PracticeQuestion1 Calculate Sum of Two Number

---
+++ PracticeQuestion1.py
@@ -0,0 +1,8 @@
+# Take the Two Number as input Print the Sum 
+
+num1 = int(input("Enter the Number 1 : "))
+num2 = int(input("Enter the Number 2 : "))
+
+sum = num1 + num2
+
+print("Sum of Two Number is : ",sum)
