ad863109748fe684df24f2ee4c0a00b13a38673b
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-24 19:28:05 +0530

    [ADDED]: shubham This code for PracticeQuestion4 Check the Greater of a & b

---
+++ PracticeQuestion4.py
@@ -0,0 +1,9 @@
+# Take 2 integer number as input a & B  and print True if a is greater than b and if not print False 
+
+a = int(input("Enter the Value of A : "))
+b = int(input("Enter the Value of B : "))
+
+if(a > b ):
+    print("TRUE")
+else:
+    print("FALSE")
