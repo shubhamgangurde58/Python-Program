f63b931fb7ea7a60921ba0aded61f8b1d227b2e9
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-24 19:26:32 +0530

    [ADDED]: shubham This code for PracticeQuestion3 Calculate the Average of Two Number

---
+++ PracticeQuestion3.py
@@ -0,0 +1,8 @@
+# Take  input 2 Floating Point Number & Print Their Average 
+
+firstNo = float(input("Enter the first No : "))
+secondNo = float(input("Enter the Second No : "))
+
+Average = (firstNo + secondNo ) / 2
+
+print("Average = ",Average)
