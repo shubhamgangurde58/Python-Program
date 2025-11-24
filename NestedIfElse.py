96a2ba0ab96d72484177ffbd6454562d552836e0
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-24 19:22:29 +0530

    [ADDED]: shubham This coe for Nested IfElse Demo Program

---
+++ NestedIfElse.py
@@ -0,0 +1,14 @@
+A = int(input("Enter the Value of A : "))
+B = int(input("Enter the Value of B : "))
+C = int(input("Enter the Value of C : "))
+
+if( A > B):
+    if(A > c):
+        print("A is Big")
+    else:
+        print("C is Big")
+else:
+    if(B > C):
+        print("B is Big")
+    else:
+        print("C is Big")
