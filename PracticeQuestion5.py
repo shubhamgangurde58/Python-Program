4dc526192c62f5c09d5ac0c74efd9e814126e2ab
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-25 23:43:22 +0530

    [ADDED]: shubham This code for Practice Question 5

---
+++ PracticeQuestion5.py
@@ -0,0 +1,11 @@
+# Take a user first name as input & print lenght of that string 
+
+string1 = input("Enter your first Name : ")
+print("Length of Input String = ",len(string1))
+
+# find the occurrence of '$'  in string 
+print("\n\n")
+
+string2 = "shubham$ jayesh$"
+
+print(string2.count("$"))
