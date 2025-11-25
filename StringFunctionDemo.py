8570f3e57a9e268ee055c75b0e2d6df188eb5c87
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-25 23:45:23 +0530

    [ADDED]: shubham This code for String & String Function Demo

---
+++ StringFunctionDemo.py
@@ -0,0 +1,44 @@
+
+# String Concatination 
+
+string1 = "shubham"
+
+string2 = "Gangurde"
+
+print(string1+" "+string2)
+
+# String Length
+
+print("string1 = ",len(string1))
+print("String2 = ",len(string2))
+
+final_str = string1 + string2
+
+print("Final_Str = ",len(final_str))
+
+# String Slicing 
+
+print(string1[3:6],"\n\n")
+
+# String Functions 
+
+myStr = "i am study in METs Institute" 
+
+# endswith
+print(myStr.endswith("tute"),"\n")
+
+#Capitalize
+print(myStr.capitalize())
+
+# replace
+print(myStr)
+print(myStr.replace("METS","IMRD"))
+print(myStr)
+
+#Find
+print(myStr.find("m"))
+
+#count
+print(myStr.count("i"))
+
+
