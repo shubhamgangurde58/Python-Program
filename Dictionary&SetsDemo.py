e0f29004529af5a11f34a2398fa13ac472a28ff0
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-27 19:02:45 +0530

    [ADDED]: shubham This code for Dictionary & Sets Demo

---
+++ Dictionary&SetsDemo.py
@@ -0,0 +1,24 @@
+myDic = {
+    "key" : "value",
+    "name" : "shubham",
+    "rollNo" : 55,
+    "city" : "shahada",
+}
+
+print(myDic)
+
+print(myDic.keys())
+
+print(myDic.values())
+
+print(myDic.get("key"))
+
+new_dic = myDic.copy()
+
+print(myDic.update(new_dic))
+
+print(new_dic)
+
+set1 = {10,20,30}
+
+print(set1)
