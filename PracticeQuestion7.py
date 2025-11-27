1b80fde7beaa78c3e84a9c5bf611174a1d35bbc1
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-27 19:03:54 +0530

    [ADDED]: shubham This code for PracticeQuestion7

---
+++ PracticeQuestion7.py
@@ -0,0 +1,30 @@
+
+# Question 1
+myDic = {
+    "table" : ["a piece of furniture ","list of fact an figure"],
+    "cat" : "a small animal "
+}
+
+print(myDic)
+
+# Qustion 2
+
+set1 = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+
+
+print("Total classroom = ",len(set1))
+
+myDic2 = {}
+
+p = int(input("Enter the Phy Marks : "))
+myDic2.update({"Phy" : p})
+
+m = int(input("Enter the math Marks : "))
+myDic2.update({"Math" : m})
+
+c = int(input("Enter the Chem Marks : "))
+myDic2.update({"Chem" : c})
+
+print(myDic2)
+
+
