a3d07137dc2c1ffb16a87389f4734ef025fdf211
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-28 19:08:28 +0530

    [ADDED]: shubham This code for while Loop Demo

---
+++ whileLoopDemo.py
@@ -0,0 +1,76 @@
+# While Loop in Python 
+
+i = 0
+
+# while (i <= 100):
+#     print(i)
+#     i += 1
+
+# while i in range(10+1):
+#     print(i)
+#     i+=1
+
+# while i in range(11):
+#     print(3*i)
+#     i+=1
+
+
+# Question 1
+# i = 0
+# while(i <= 10):
+#     print(i)
+#     i+=1
+
+
+# Question 2
+# n = int(input("Enter the value of N : "))
+# i=0
+# sum = 0
+# while(i<=n):
+#     sum += i
+#     i += 1
+
+# print("sum = ",sum)
+
+# Question 3
+
+# num = int(input("Enter the number to print the table : "))
+# i = 1
+# while(i < 10):
+#     print(num," x ",i," = ",i*num )
+#     i += 1
+
+
+# Question 4
+
+# num = int(input("enter the any number : "))
+# rev = 0
+
+# while(num > 0):
+
+#     reminder = num % 10
+#     rev = rev * 10 + reminder
+#     num = num // 10
+
+# print(rev)
+
+
+
+# Question 5
+
+num = int(input("Enter the any number : "))
+count = 0
+
+while num > 0:
+    num = num // 10
+    count += 1
+
+
+print("count = ",count)
+
+
+
+
+
+
+
