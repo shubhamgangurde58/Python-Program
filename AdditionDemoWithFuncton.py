043ba5fc03451f623ce1a173941600cf39e178bd
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-01-08 22:01:25 +0530

    [ADDED]: shubham This code for AdditionDemoWithFunction

---
+++ AdditionDemoWithFuncton.py
@@ -0,0 +1,10 @@
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+ans = add_numbers(num1, num2)
+
+print("Addition is:", ans)
