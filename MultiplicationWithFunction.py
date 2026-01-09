57a782824d968679a5a867ecf6ebc424bbc07ac8
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-01-09 21:27:03 +0530

    [ADDED]: shubham This code for Multiplication With Function Demo

---
+++ MultiplicationWithFunction.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    return a * b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+ans = multiply(num1, num2)
+
+print("Multiplication is:", ans)
